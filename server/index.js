992c62d085a3fe137da29195a144733db05104ac
Mark0S1mic <simicmarko15@yahoo.com>
2025-07-21 16:40:42 +0200

    Dodat dockerfile

--- server/index.js
+++ server/index.js
@@ -25,4 +25,7 @@ mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnified
 .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))) 
 .catch((error) => console.log(error.message));
 
+mongoose.connection.once('open', () => {
+  console.log('✅ MongoDB connected!');
+});
 
